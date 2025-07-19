e707ae98430e825f13a8ee0d968d2bd677f34c56
junichihattori <junhat6@gmail.com>
2025-07-19 21:47:36 +0900

    generate:apiの依存関係の不足の解決

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,6 +38,9 @@ dependencies {
 	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
 	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
 
+	// --- OpenAPI/Swagger ドキュメント生成 ---
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
+
 	// --- テスト用のライブラリ ---
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")

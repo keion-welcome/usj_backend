66ac19964a70e9671753b1d02c2a0d5c58f3b85a
junichihattori <junhat6@gmail.com>
2025-09-06 11:18:56 +0900

    DTOにバリデーションチェックを追加

--- src/main/kotlin/com/example/backend/api/dto/request/LoginRequest.kt
+++ src/main/kotlin/com/example/backend/api/dto/request/LoginRequest.kt
@@ -1,8 +1,15 @@
 package com.example.backend.api.dto.request
+
+import jakarta.validation.constraints.*
+
 /**
  * クライアントからのログインリクエストDTO。
  */
 data class LoginRequest(
+    @field:NotBlank
+    @field:Email
     val email: String,
+    
+    @field:NotBlank
     val password: String
 )
